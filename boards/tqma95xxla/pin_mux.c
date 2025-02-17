5d31271a4c12d612f31547d07f57928c1e84b003
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2025-02-17 13:35:14 +0100

    boards: add tqma95xxla
    Start as a copy from tqma95xxsa but use LPUART2 as debug UART.
    Follow reference boards where LPUART2 is used for OEI and SM
    and LPUART1 is for Cortex-A55 cluster.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

---
+++ boards/tqma95xxla/pin_mux.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ * Copyright (c) 2025 TQ-Systems GmbH <oss@tq-group.com>, D-82229 Seefeld, Germany.
+ */
+
+#include "pin_mux.h"
+#include "board.h"
+
+/* FUNCTION ************************************************************************************************************
+ *
+ * Function Name : BOARD_InitPins
+ * Description   : Configures pin routing and optionally pin electrical features.
+ *
+ * END ****************************************************************************************************************/
+void BOARD_InitPins(void)
+{
+#if (BOARD_DEBUG_UART_INSTANCE == 1U)
+    /* Configure LPUART 1 not supported */
+    #error
+#elif (BOARD_DEBUG_UART_INSTANCE == 2U)
+    /* Configure LPUART 2 */
+    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_RXD__LPUART2_RX, 0U);
+    IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_RXD__LPUART2_RX, IOMUXC_PAD_PD(1U));
+
+    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 0);
+    IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_TXD__LPUART2_TX, IOMUXC_PAD_DSE(0xFU));
+#endif
+}
