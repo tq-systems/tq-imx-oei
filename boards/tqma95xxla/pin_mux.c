e527e3ec3229a0668f1883523693534a22a39b74
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2026-03-06 16:01:55 +0100

    tqma95xxla: pin_mux: improve preprocessor guard
    Pin muxing for debug uart is guarded with if, let compiling fail
    in any case when condition do not match.
    
    While at it, force second paramter of IOMUXC_SetPinMux to unsigned
    for all occurences.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- boards/tqma95xxla/pin_mux.c
+++ boards/tqma95xxla/pin_mux.c
@@ -15,15 +15,15 @@
  * END ****************************************************************************************************************/
 void BOARD_InitPins(void)
 {
-#if (BOARD_DEBUG_UART_INSTANCE == 1U)
-    /* Configure LPUART 1 not supported */
-    #error
-#elif (BOARD_DEBUG_UART_INSTANCE == 2U)
+#if (BOARD_DEBUG_UART_INSTANCE == 2U)
     /* Configure LPUART 2 */
     IOMUXC_SetPinMux(IOMUXC_PAD_UART2_RXD__LPUART2_RX, 0U);
     IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_RXD__LPUART2_RX, IOMUXC_PAD_PD(1U));
 
-    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 0);
+    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 0U);
     IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_TXD__LPUART2_TX, IOMUXC_PAD_DSE(0xFU));
+#else
+    /* BOARD_DEBUG_UART_INSTANCE configuration is not supported for this board */
+#error
 #endif
 }
